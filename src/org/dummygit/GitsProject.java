0680dc9f4f21669ab1ee83f093eb4f17d76f41a9
Vigneshramm2021 <vignesh.ram@yahoo.com>
2021-11-10 06:37:46 +0530

    Class Timing details

---
+++ src/org/dummygit/GitsProject.java
@@ -0,0 +1,12 @@
+package org.dummygit;
+
+public class GitsProject {
+	public static void classtiming() {
+		System.out.println("Class timing is 9'Oclock");
+	}
+
+	public static void main(String[] args) {
+		classtiming();
+	}
+
+}
