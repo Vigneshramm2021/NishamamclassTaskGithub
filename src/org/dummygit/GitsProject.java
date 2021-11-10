1bfc996b2cc3b15b0aa1aaf1039bb72693973a1d
Vigneshramm2021 <vignesh.ram@yahoo.com>
2021-11-10 06:53:39 +0530

    just ssssss added

--- src/org/dummygit/GitsProject.java
+++ src/org/dummygit/GitsProject.java
@@ -1,12 +1,12 @@
 package org.dummygit;
 
 public class GitsProject {
-	public static void classtiming() {
+	public static void classtimingssssssssssssss() {
 		System.out.println("Class timing is 9'Oclock");
 	}
 
 	public static void main(String[] args) {
-		classtiming();
+		classtimingssssssssssssss();
 	}
 
 }
