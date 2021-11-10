badeb4ec106136b61728cd0be629f1b0b3a469ca
Vigneshramm2021 <vignesh.ram@yahoo.com>
2021-11-10 06:50:10 +0530

    root timing

--- src/org/dummygit/GitsProject.java
+++ src/org/dummygit/GitsProject.java
@@ -1,12 +1,12 @@
 package org.dummygit;
 
 public class GitsProject {
-	public static void classtiming() {
-		System.out.println("Class timing is 9'Oclock");
+	public static void roottiming() {
+		System.out.println("12'Oclock");
 	}
 
 	public static void main(String[] args) {
-		classtiming();
+		roottiming();
 	}
 
 }
